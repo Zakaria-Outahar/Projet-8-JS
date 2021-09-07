050d1e174b7ce78df65bdeba899e1e0ff89d80e5
Zakaria Outahar <outaharz@gmail.com>
2021-09-07 15:53:21 +0200

    JavaScript : apparition initiale statique

---
+++ app.js
@@ -0,0 +1,17 @@
+const inputsCouleur = document.querySelectorAll('.inp-couleur');
+const inputRange = document.querySelector('.inp-range');
+const btns = document.querySelectorAll('button');
+const fond = document.body;
+
+// Démarrage
+
+let valCouleurs = ['#ba5370', '#f4e2d8'];
+let inclinaison = 45;
+
+inputsCouleur[0].value = valCouleurs[0];
+inputsCouleur[1].value = valCouleurs[1];
+inputsCouleur[0].style.background = valCouleurs[0];
+inputsCouleur[1].style.background = valCouleurs[1];
+fond.style.background = `linear-gradient(${inclinaison}deg, ${inputsCouleur[0].value},${inputsCouleur[1].value})`;
+
+
