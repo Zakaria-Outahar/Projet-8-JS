2506f83a90b5c1f7f8d03a4a1e0eaa8f7425c52b
Zakaria Outahar <outaharz@gmail.com>
2021-09-08 09:06:38 +0200

    JavaScript : générer aléatoirement des gradients

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const containerCouleurs = document.querySelector('.container-couleurs');
 const span = document.querySelector('span');
 const btnRandom = document.querySelector('.random'); 
 
-
 // Démarrage
 
 let valCouleurs = ['#BA5370', '#F4E2D8'];
@@ -17,7 +16,7 @@ inputsCouleur[0].value = valCouleurs[0];
 inputsCouleur[1].value = valCouleurs[1];
 inputsCouleur[0].style.background = valCouleurs[0];
 inputsCouleur[1].style.background = valCouleurs[1];
-fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleurs[0]},${valCouleurs[1]})`;
+fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleurs})`;
 
 
 // Inclinaison
@@ -36,7 +35,16 @@ btns.forEach(btn => {
 function rajouteEnleve(e){
     span.innerText = "";
     const allInputs = document.querySelectorAll('.inp-couleur');
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    if(randomColor.length < 6 || randomColor.length >= 7){
+        if(randomColor.length === 5){
+            randomColor = `${randomColor}${Math.floor(Math.random()*17).toString(16)}`;
+        } else if(randomColor.length === 4){
+            randomColor = `${randomColor}${Math.floor(Math.random()*17).toString(16)}${Math.floor(Math.random()*17).toString(16)}`;
+        } else if(randomColor.length === 7){
+            randomColor = randomColor.slice(0,6);
+        }
+    }
     if(e.target.className === "plus"){
         if(allInputs.length >= 7){
             return;
@@ -64,4 +72,27 @@ function rajouteEnleve(e){
         }
     }
 }
+
+// Couleur aléatoire
+
+btnRandom.addEventListener('click', () => {
+    span.innerText = "";
+    const inputs = document.querySelectorAll('.inp-couleur');
+    for (let i = 0; i < valCouleurs.length; i++) {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        if(randomColor.length < 6){
+            if(randomColor.length === 5){
+                randomColor = `${randomColor}${Math.floor(Math.random()*17).toString(16)}`;
+            } else if(randomColor.length === 4){
+                randomColor = `${randomColor}${Math.floor(Math.random()*17).toString(16)}${Math.floor(Math.random()*17).toString(16)}`;
+            } else if(randomColor.length === 7){
+                randomColor = randomColor.slice(0,6);
+            }
+        }
+        valCouleurs[i] = `#${randomColor.toUpperCase()}`;
+        inputs[i].value = valCouleurs[i];
+        inputs[i].style.background = valCouleurs[i];
+    }
+    fond.style.background = `linear-gradient(${inclinaison}deg, ${valCouleurs})`;
+})
 
